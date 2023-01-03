31830057c69be46ee0ca759e222e6f5a7a694e9c
Mounzer Adas <mounzer.dev@gmail.com>
2023-01-03 22:34:36 +0200

    feat(export): better export

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,10 +1,10 @@
 import { describe, expect, it } from "vitest";
-import { Codefend } from "../index";
+import { obfuscate } from "../index";
 
 describe("parser()", () => {
   const code = "const l_a = 0;";
   it("Obfuscate with default options", () => {
-    const output = Codefend.obfuscate(code);
+    const output = obfuscate(code);
     expect(output).toEqual("const Ox0 = 0;");
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -6,23 +6,22 @@ import {
 import { CodefendParser } from "./core/parser/CodeDefendParser";
 import { CodefendReplacer } from "./core/replacer/CodeDefendReplacer";
 
-export const Codefend = {
-  core: {
-    parser: new CodefendParser(defaultOptions),
-    mapper: new CodefendMapper(defaultOptions),
-    replacer: new CodefendReplacer(),
-  },
-  obfuscate: (
-    code: string,
-    map: Record<string, string> = {},
-    options?: ICodefendOptions
-  ) => {
-    const words = Codefend.core.parser.parse(code, options?.regexList);
-    Codefend.core.mapper.buildMap(words, map, options?.prefix);
-    map = Codefend.core.mapper.sortMap(map);
-    Codefend.core.mapper.mapPredefinedWords(map, options?.predefinedWords);
-    Codefend.core.mapper.mapIgnoredWords(map, options?.ignoredWords);
-    const output = Codefend.core.replacer.replace(map, code);
-    return output;
-  },
+export const CodefendCore = {
+  parser: new CodefendParser(defaultOptions),
+  mapper: new CodefendMapper(defaultOptions),
+  replacer: new CodefendReplacer(),
 };
+
+export function obfuscate(
+  code: string,
+  map: Record<string, string> = {},
+  options?: ICodefendOptions
+) {
+  const words = CodefendCore.parser.parse(code, options?.regexList);
+  CodefendCore.mapper.buildMap(words, map, options?.prefix);
+  map = CodefendCore.mapper.sortMap(map);
+  CodefendCore.mapper.mapPredefinedWords(map, options?.predefinedWords);
+  CodefendCore.mapper.mapIgnoredWords(map, options?.ignoredWords);
+  const output = CodefendCore.replacer.replace(map, code);
+  return output;
+}
