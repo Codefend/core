7969fc52fe22a9dd09eb8ea209088d8d125da723
Mounzer Adas <mounzer.dev@gmail.com>
2023-01-03 22:51:20 +0200

    test(replacer): update

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -29,7 +29,7 @@ describe("Replacer", () => {
   const code = "const l_a = 0;";
   const map = { l_a: "Ox0" };
   it("with default options", () => {
-    const output = CodefendCore.replacer.replace(map, code);
+    const output = CodefendCore.replacer.replace(code, map);
     expect(output).toEqual("const Ox0 = 0;");
   });
 });
