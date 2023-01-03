bb0c3ad99a754cd2b81c45bc40404970100fcf42
Mounzer Adas <mounzer.dev@gmail.com>
2023-01-03 22:48:04 +0200

    test(core): add default option tests

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,10 +1,35 @@
 import { describe, expect, it } from "vitest";
-import { obfuscate } from "../index";
+import { obfuscate, CodefendCore } from "../index";
 
-describe("parser()", () => {
+describe("Obfuscate", () => {
   const code = "const l_a = 0;";
-  it("Obfuscate with default options", () => {
+  it("with default options", () => {
     const output = obfuscate(code);
+    expect(output).toEqual("const Ox0 = 0;");
+  });
+});
+
+describe("Parser", () => {
+  const code = "const l_a = 0;";
+  it("with default options", () => {
+    const words = CodefendCore.parser.parse(code);
+    expect(words).toEqual([{ value: "l_a", regexIndex: 1 }]);
+  });
+});
+
+describe("Mapper", () => {
+  const words = [{ value: "l_a", regexIndex: 1 }];
+  it("with default options", () => {
+    const map = CodefendCore.mapper.buildMap(words, {}, "Ox");
+    expect(map).toEqual({ l_a: "Ox0" });
+  });
+});
+
+describe("Replacer", () => {
+  const code = "const l_a = 0;";
+  const map = { l_a: "Ox0" };
+  it("with default options", () => {
+    const output = CodefendCore.replacer.replace(map, code);
     expect(output).toEqual("const Ox0 = 0;");
   });
 });
