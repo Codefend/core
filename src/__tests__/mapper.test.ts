425c93fd1b6b7c5162dd98ef6a4a3908828f38cb
Mounzer Adas <mounzer.dev@gmail.com>
2023-01-04 07:49:01 +0200

    test(mapper): add predefined and ignored words

--- src/__tests__/mapper.test.ts
+++ src/__tests__/mapper.test.ts
@@ -1,4 +1,5 @@
 import { describe, expect, it } from "vitest";
+import { ICodefendPredefinedWordOption } from "../core/options/ICodeDefendOptions";
 import { CodefendCore } from "../index";
 
 describe("Mapper", () => {
@@ -20,6 +21,61 @@ describe("Mapper", () => {
       map = CodefendCore.mapper.sortMap(map);
       expect(JSON.stringify(map)).toEqual(
         JSON.stringify({ "lib-file": "Ox1", l_var: "Ox0" })
+      );
+    });
+  });
+
+  describe("Ignored words", () => {
+    const ignoredWords = ["l_ignored"];
+    let map: Record<string, string> = {
+      l_var: "Ox0",
+      "lib-file": "Ox1",
+      l_ignored: "Ox2",
+    };
+
+    it("should ignore the word", () => {
+      map = CodefendCore.mapper.mapIgnoredWords(map, ignoredWords);
+      expect(map[ignoredWords[0]]).toEqual(ignoredWords[0]);
+    });
+  });
+
+  describe("Predefined words", () => {
+    const predefinedWords: ICodefendPredefinedWordOption[] = [
+      { originalWord: "l_predefined", targetWord: "predefinedTarget" },
+    ];
+    let map: Record<string, string> = {
+      l_var: "Ox0",
+      "lib-file": "Ox1",
+      l_predefined: "Ox2",
+    };
+
+    it("should replace with the target word", () => {
+      map = CodefendCore.mapper.mapPredefinedWords(map, predefinedWords);
+      expect(map[predefinedWords[0].originalWord]).toEqual(
+        predefinedWords[0].targetWord
+      );
+    });
+  });
+
+  describe("Predefined and Ignored words", () => {
+    const ignoredWords = ["l_predefined_and_ignored"];
+    const predefinedWords: ICodefendPredefinedWordOption[] = [
+      {
+        originalWord: "l_predefined_and_ignored",
+        targetWord: "predefinedTarget",
+      },
+    ];
+    let map: Record<string, string> = {
+      l_var: "Ox0",
+      "lib-file": "Ox1",
+      l_predefined_and_ignored: "Ox2",
+    };
+
+    it("ignore > predefined", () => {
+      map = CodefendCore.mapper.mapIgnoredWords(map, ignoredWords);
+      map = CodefendCore.mapper.mapPredefinedWords(map, predefinedWords);
+      expect(map[predefinedWords[0].originalWord]).toEqual(
+        predefinedWords[0].targetWord
       );
     });
   });
