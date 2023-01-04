fd7ad004cb2c8bf8d121faa81a5029935081c2ce
Mounzer Adas <mounzer.dev@gmail.com>
2023-01-04 06:27:49 +0200

    test(regexlist): update test to new regexList model

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -13,12 +13,12 @@ describe("Parser", () => {
   const code = "const l_a = 0;";
   it("with default options", () => {
     const words = CodefendCore.parser.parse(code);
-    expect(words).toEqual([{ value: "l_a", regexIndex: 1 }]);
+    expect(words).toEqual([{ value: "l_a", fromRegex: "main" }]);
   });
 });
 
 describe("Mapper", () => {
-  const words = [{ value: "l_a", regexIndex: 1 }];
+  const words = [{ value: "l_a", fromRegex: "main" }];
   it("with default options", () => {
     const map = CodefendCore.mapper.buildMap(words, {}, "Ox");
     expect(map).toEqual({ l_a: "Ox0" });
