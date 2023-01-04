3d0f35219b0ab845cc665a87559c33c7dcd1fee2
Mounzer Adas <mounzer.dev@gmail.com>
2023-01-04 06:59:42 +0200

    test(obfuscate): add file/main/all

--- src/__tests__/obfuscate.test.ts
+++ src/__tests__/obfuscate.test.ts
@@ -2,9 +2,27 @@ import { describe, expect, it } from "vitest";
 import { obfuscate } from "../index";
 
 describe("Obfuscate", () => {
-  const code = "const l_a = 0;";
-  it("with default options", () => {
-    const output = obfuscate(code);
-    expect(output).toEqual("const Ox0 = 0;");
+  describe("main-regex", () => {
+    const code = "const l_a = 0;";
+    it("with default options", () => {
+      const output = obfuscate(code);
+      expect(output).toEqual("const Ox0 = 0;");
+    });
+  });
+
+  describe("file-regex", () => {
+    const code = `import lib_utils from "./lib-utils";`;
+    it("with default options", () => {
+      const output = obfuscate(code);
+      expect(output).toEqual(`import Ox0 from "./Ox1";`);
+    });
+  });
+
+  describe("all-regex", () => {
+    const code = `import lib_utils from "./lib-utils";const l_a = 0;`;
+    it("with default options", () => {
+      const output = obfuscate(code);
+      expect(output).toEqual(`import Ox0 from "./Ox2";const Ox1 = 0;`);
+    });
   });
 });
