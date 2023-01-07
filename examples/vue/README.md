1a69eda49dac4a02baa75b6ffe23d10dd2481695
Z-n-o-M <mounzer.dev@gmail.com>
2023-01-07 16:29:47 +0200

    docs(examples): vue

---
+++ examples/vue/README.md
@@ -0,0 +1,25 @@
+# Codefend Vue example
+
+## Usage
+
+```bash
+npm i -g codefend@latest // or npm i -D codefend@latest
+codefend -i
+codefend -o
+cd codefend-output
+code .
+```
+
+<p align="center">
+ <img src="../../public/img/npx_example_1.PNG">
+</p>
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ examples/vue/app.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { ref, computed } from "vue";
+
+/**
+ * ref
+ */
+const ref_message = ref("Hello World");
+
+/**
+ * computed
+ */
+const ref_count = ref(5);
+const computed_count = computed(() => {
+    return ref_count.value + 5;
+});
+
+/**
+ * function
+ */
+function f_onButtonClicked() {
+    console.log("clicked");
+}
+</script>
+<template>
+    <div class="css_count">
+        {{ computed_count }}
+    </div>
+    <div class="css_message">
+        {{ ref_message }}
+    </div>
+    <button @click="f_onButtonClicked">Click me!</button>
+</template>
+<style lang="css" scoped>
+.css_count {
+    color: red;
+}
+
+.css_message {
+    color: blue;
+}
+</style>
