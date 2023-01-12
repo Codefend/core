869d160175bbe3dc3e99d234b93b2e73d151b125
Z-n-o-M <41215398+Z-n-o-M@users.noreply.github.com>
2023-01-13 00:35:00 +0200

    feat(debug): increase message size

--- src/utils/Constants.ts
+++ src/utils/Constants.ts
@@ -2,7 +2,7 @@ export const OPTIONS_FILE_PATH = "./.codefendrc.json";
 
 export const LOG_OPTIONS = {
   PREFIX_SIZE: 9,
-  MESSAGE_SIZE: 60,
+  MESSAGE_SIZE: 64,
   COLORS: {
     PREFIX_BG: "#282634",
     PREFIX_FG: "#ff4067",
