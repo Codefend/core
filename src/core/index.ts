d51727e37b22001a4edb1d04c64086d9e1c1664b
Z-n-o-M <mounzer.dev@gmail.com>
2023-02-12 16:21:30 +0200

    refactor(import): index

---
+++ src/core/index.ts
@@ -0,0 +1,7 @@
+export { buildMap, sortMap } from "./mapper";
+export { mapIgnoredWords } from "./ignored";
+export { mapPredefinedWords } from "./predefined";
+export { obfuscate } from "./obfuscate";
+export { buildObfuscateOptions } from "./obfuscate";
+export { buildDefaultOptions } from "./options";
+export { buildRuntimeOptions } from "./runtime";
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
-export { buildObfuscateOptions } from "./core/obfuscate";
-export { buildDefaultOptions } from "./core/options";
-export { buildRuntimeOptions } from "./core/runtime";
-export { mapIgnoredWords } from "./core/ignored";
-export { mapPredefinedWords } from "./core/predefined";
-export { obfuscate } from "./core/obfuscate";
-export * as log from "./common/log";
+export {
+  buildObfuscateOptions,
+  buildDefaultOptions,
+  buildRuntimeOptions,
+  mapIgnoredWords,
+  mapPredefinedWords,
+  obfuscate,
+} from "./core";
+export { log } from "./common/index";
 export { startCLI } from "./cli";
