830e7a6141bde181c03f0184bced3bd01e2a0b6d
Z-n-o-M <mounzer.dev@gmail.com>
2024-08-04 00:06:39 -0700

    fix(check): adjust faulty `package-lock` warning (#181)

--- src/models/types.ts
+++ src/models/types.ts
@@ -1,7 +1,9 @@
-import { CODEFEND_CHECK_ERROR_CODES, CODEFEND_CHECK_WARNING_CODES } from "../core/utils/constants";
+import { CODEFEND_CHECK_ERROR_CODES, CODEFEND_CHECK_WARNING_CODES, PARSER_NAMES } from "../core/utils/constants";
 
 export type ICheckErrorCodes = keyof typeof CODEFEND_CHECK_ERROR_CODES;
 
 export type ICheckWarningCodes = keyof typeof CODEFEND_CHECK_WARNING_CODES;
 
 export type IStringModifierFunction = (word: string) => string;
+
+export type IParserNames = keyof typeof PARSER_NAMES | "custom";
